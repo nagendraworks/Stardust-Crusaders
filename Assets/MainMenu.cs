61b6ae48c791b4cb60be0dedc866092218c053bb
Light#0001 <152773601+y8done@users.noreply.github.com>
2025-01-24 09:32:33 +0530

    done

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,6 +1,6 @@
- using UnityEngine;
+using UnityEngine;
 
-public class CombatScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
