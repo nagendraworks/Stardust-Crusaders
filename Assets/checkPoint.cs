193e72211971422b2bcaad4f11d6d76ad9241823
Vaibhav04749 <dodiyavaibhav00@gmail.com>
2025-01-24 18:27:21 +0530

    Revert "Ae hallo"
    This reverts commit b3d7b61805cfbcc6248c4bca32d0d7f8be7a6faa.

---
+++ Assets/checkPoint.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class checkPoint : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider2D collision)
+    {
+        if (collision.CompareTag("character"))
+        {
+            //go
+        }
+    }
+}
